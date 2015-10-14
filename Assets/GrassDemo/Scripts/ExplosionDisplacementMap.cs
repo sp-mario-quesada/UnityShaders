65d6ec1c55d8d3ced6c3d418241e44d3c372239e
Mario <centralmqmtech@gmail.com>
2015-10-14 05:10:38 +0200

    added post processes

---
+++ Assets/GrassDemo/Scripts/ExplosionDisplacementMap.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class ExplosionDisplacementMap : MonoBehaviour 
+{
+	Material _material;
+
+	void CreateAssets()
+	{
+		if(_material == null)
+		{
+			_material = Resources.Load<Material>("ExplosionDisplacementMapMat");
+		}
+	}
+
+	void OnRenderImage(RenderTexture src, RenderTexture dest)
+	{
+		CreateAssets();
+
+		Graphics.Blit(src, dest, _material);
+	}
+}
